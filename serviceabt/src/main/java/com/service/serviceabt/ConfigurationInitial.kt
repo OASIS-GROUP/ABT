e919ebb92245bf05726e9da43f81884d3cff6434
OASIS-NEST <angel.galvez2789@gmail.com>
2025-07-21 20:22:56 -0500

    Upload project

--- serviceabt/src/main/java/com/service/serviceabt/ConfigurationInitial.kt
+++ serviceabt/src/main/java/com/service/serviceabt/ConfigurationInitial.kt
@@ -1,26 +1,9 @@
 package com.service.serviceabt
 
-import android.content.Context
-import android.util.AttributeSet
-import android.widget.LinearLayout
-import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.TextView
 
+class ConfigurationInitial {
 
-class ConfigurationInitial @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-) : LinearLayout(context, attrs) {
-
-    init {
-        LayoutInflater.from(context).inflate(R.layout.activity_configuration_initial, this, true)
-        val btn = findViewById<Button>(R.id.btn_initial)
-        val text = findViewById<TextView>(R.id.txt_name)
-
-        btn.setOnClickListener {
-            text.text = "Hola"
-        }
-
+    fun processUUID (uuid : String){
+        println("UUID: $uuid")
     }
 }
